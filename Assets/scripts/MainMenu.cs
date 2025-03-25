a769c9225a75c1937e81a3fc9ef55627cdc6ae70
Nomis1123 <72155251+Nomis1123@users.noreply.github.com>
2025-03-25 15:41:13 -0400

    resized level 1 and implemented level traversal from hub to level one and back

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -8,6 +8,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Playlvl1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Hubfromlvl1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+
 
     public void QuitGame()
     {
